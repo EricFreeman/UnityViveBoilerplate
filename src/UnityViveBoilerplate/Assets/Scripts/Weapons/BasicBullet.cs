dcc5fe4578bba5ce60d8926ec91b9bfac0a8f845
Eric Freeman <clownhunter@gmail.com>
2017-01-20 18:14:56 -0700

    More work on BasicGun and BasicBullet.

---
+++ src/UnityViveBoilerplate/Assets/Scripts/Weapons/BasicBullet.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Policy;
+using UnityEngine;
+
+namespace Assets.Scripts.Weapons
+{
+    public class BasicBullet : MonoBehaviour
+    {
+        public float Speed = 3f;
+        public float Damage = 1;
+        public float ImpactForce = 10f;
+
+        void Update()
+        {
+            transform.Translate(0, 0, Speed * Time.deltaTime);
+        }
+
+        void OnTriggerEnter(Collider collider)
+        {
+            var rigidbody = collider.GetComponent<Rigidbody>();
+            if (rigidbody)
+            {
+                rigidbody.AddForce(transform.forward * ImpactForce, ForceMode.Impulse);
+            }
+
+            Destroy(gameObject);
+        }
+    }
+}
--- src/UnityViveBoilerplate/Assets/Scripts/Weapons/BasicGun.cs
+++ src/UnityViveBoilerplate/Assets/Scripts/Weapons/BasicGun.cs
@@ -1,29 +1,87 @@
 ﻿using System.Collections;
 using Assets.Scripts.Common;
 using UnityEngine;
-using Valve.VR;
 
 namespace Assets.Scripts.Weapons
 {
     public class BasicGun : MonoBehaviour
     {
-        public float ForceAmount = .25f;
-        public float ForceDuration = .05f;
+        public GameObject BulletPrefab;
+
+        public int ClipSize = 12;
+        private int _currentClip;
+
+        public float CooldownSpeed = .25f;
+        public float _currentCooldownSpeed;
+
+        public float RumbleForceAmount = .25f;
+        public float RumbleForceDuration = .05f;
+
+        public float EmptyRumbleForceAmount = .15f;
+        public float EmptyRumbleForceDuration = .025f;
 
         private PickupOptions _pickupOptions;
+        private GunState _gunState = GunState.ReadyToFire;
 
         void Start()
         {
             _pickupOptions = GetComponent<PickupOptions>();
+            _currentClip = ClipSize;
         }
 
         void Update()
         {
             if (_pickupOptions.Controller == null) return;
 
+            UpdateRumble();
+
+            if (_gunState == GunState.ReadyToFire)
+            {
+                LoadedUpdate();
+            }
+            else if (_gunState == GunState.CoolingDown)
+            {
+                CoolingDownUpdate();
+            }
+        }
+
+        private void UpdateRumble()
+        {
             if (_pickupOptions.Controller.GetHairTriggerDown())
             {
-                StartCoroutine(RumbleController(ForceDuration, ForceAmount));
+                StartCoroutine(RumbleController(
+                    _gunState == GunState.ReadyToFire ? RumbleForceDuration : EmptyRumbleForceDuration,
+                    _gunState == GunState.ReadyToFire ? RumbleForceAmount : EmptyRumbleForceAmount));
+            }
+        }
+
+        private void LoadedUpdate()
+        {
+            if (_pickupOptions.Controller.GetHairTriggerDown())
+            {
+                var bullet = Instantiate(BulletPrefab);
+                var tip = transform.Find("Tip");
+                bullet.transform.position = tip.transform.position;
+                bullet.transform.rotation = tip.transform.rotation;
+
+                _currentClip--;
+                if (_currentClip <= 0)
+                {
+                    _gunState = GunState.Empty;
+                    return;
+                }
+
+                _gunState = GunState.CoolingDown;
+                _currentCooldownSpeed = CooldownSpeed;
+            }
+        }
+
+        private void CoolingDownUpdate()
+        {
+            _currentCooldownSpeed -= Time.deltaTime;
+            if (_currentCooldownSpeed <= 0)
+            {
+                _gunState = GunState.ReadyToFire;
             }
         }
 
@@ -35,7 +93,6 @@ namespace Assets.Scripts.Weapons
         IEnumerator RumbleController(float duration, float strength)
         {
             strength = Mathf.Clamp01(strength);
-            var startTime = Time.realtimeSinceStartup;
 
             for (float i = 0; i < duration; i += Time.deltaTime)
             {
@@ -44,5 +101,12 @@ namespace Assets.Scripts.Weapons
                 yield return null;
             }
         }
+    }
+
+    public enum GunState
+    {
+        ReadyToFire,
+        CoolingDown,
+        Empty
     }
 }
